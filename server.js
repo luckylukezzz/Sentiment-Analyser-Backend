a39f50c3a68031f17c3ec2094e15b940a5a48eaa
patalee narasinghe <patalee2001@gmail.com>
2024-07-04 18:12:39 +0530

    port changes to 5000

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cors())
 app.use("/api/v1", mainRouter);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
 
