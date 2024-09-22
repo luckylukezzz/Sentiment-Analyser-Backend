7447e0585b937af2bf48f0d72a2c15f50826010e
patalee narasinghe <patalee2001@gmail.com>
2024-09-22 13:21:47 +0530

    check the cicd

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const start = async () => {
     try {        
         await connectDB(process.env.MONGO_URI);
         app.listen(port, () => {
-            console.log(`Server is listening on port ${port}`);
+            console.log(`Serverzzz is listening on port ${port}`);
         })
 
     } catch (error) {
