40972c756ea3450c675b3da70f567970b0c597a1
patalee narasinghe <patalee2001@gmail.com>
2024-09-24 17:08:36 +0530

    created endpoint for lime explanations dashboard/limeInfo?asin=B00NQHZ2RU

---
+++ controllers/lime.js
@@ -0,0 +1,32 @@
+const getLimeInfo = async (req, res) => {
+    const pool = req.mysqlPool;
+    const parentAsin = req.query.parentAsin;
+    console.log(parentAsin)
+    if (!parentAsin) {
+        return res.status(400).json({ error: 'Parent ASIN is required' });
+    }
+
+    try {
+        const [reviews] = await pool.query(
+            "SELECT review_id, text, lime FROM reviews WHERE parent_asin = ? AND lime IS NOT NULL",
+            [parentAsin]
+        );
+
+        const newJsonStructure = {};
+        reviews.forEach((review, index) => {
+            console.log(review.lime)
+            const reviewKey = `review${index + 1}`;
+            const limeData = review.lime;
+            newJsonStructure[reviewKey] = {
+                ...limeData
+            };
+        });
+
+        res.json(newJsonStructure);
+    } catch (error) {
+        console.error('Error fetching reviews:', error);
+        res.status(500).json({ error: 'An error occurred while fetching reviews' });
+    }
+};
+
+module.exports = { getLimeInfo };
