803a639dd1780f67b051e6a59ef9e908e22d9e7e
patalee narasinghe <patalee2001@gmail.com>
2024-10-14 11:54:45 +0530

    added tests for login registration

--- server.js
+++ server.js
@@ -9,13 +9,11 @@ const start = async () => {
         await createMySQLPool(); // Initialize MySQL pool
         await connectDB(process.env.MONGO_URI); // Connect to MongoDB
         console.log("Connected to MongoDB");
-
-        // Only start the server if not in test environment
-        if (process.env.NODE_ENV !== 'test') {
-            app.listen(port, () => {
-                console.log(`Server is listening on port ${port}`);
-            });
-        }
+        
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}`);
+        });
+        
     } catch (error) {
         console.error('Error starting the server:', error.message);
     }
